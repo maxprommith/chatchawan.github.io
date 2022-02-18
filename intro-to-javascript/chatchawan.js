a65f7b3c75d7993d8637a1c2ebb0502ae0a83228
maxprommith <max.prommith@gmail.com>
2022-02-18 13:58:23 +0700

    add my age and load profile

--- intro-to-javascript/chatchawan.js
+++ intro-to-javascript/chatchawan.js
@@ -1,11 +1,25 @@
 function askMyName() {
-    const name = prompt("Please enter your name",);
+    const name = prompt("Please enter your name");
     if (name != null) {
        document.getElementById("name").innerHTML = "My name is " + name;
     }
  }
  
  function helloWorld() {
- const h1message = document.getElementById("name").innerHTML
-    alert(h1message + ' เลิกกับพี่ ได้ดีทุกคน')
+   const h1message = document.getElementById("name").innerHTML
+      alert(h1message + ' เลิกกับพี่ ได้ดีทุกคน')                   
  }
+
+ function calAge() {
+   const BirthYear = Number(prompt("กรอกปีเกิดของคุณ (ค.ศ.) "));
+   const year = new Date().getFullYear()
+   const age = year - BirthYear
+   alert(`ยินดีด้วย! คุณอยู่บนโลกนี้มา ${age} ปีแล้ว`)
+}
+
+function loadProfile() {
+   const name = "Chatchawan"
+   const province = "Chiang Mai"
+   document.getElementById("my-name").innerHTML = name
+   document.getElementById("my-province").innerHTML = province
+}
